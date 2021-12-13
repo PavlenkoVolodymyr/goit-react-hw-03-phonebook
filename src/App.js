e2b31f94256e719e7dfd8b1c4af55e3b8bdd3003
PavlenkoVolodymyr <volodymyrp88@gmail.com>
2021-12-13 18:32:39 +0200

    ht3

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import Container from './components/Container/Container';
 import Phonebook from './components/Phonebook/Phonebook';
 import Contacts from './components/Contacts/Contacts';
 import Filter from './components/Filter/Filter';
+import { getFromLS, setInLS } from './utilits/localStorage';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = getFromLS('contacts');
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      setInLS('contacts', contacts);
+    }
+  }
 
   onAddContact = (name, number) => {
     if (this.onCheckContact(name)) {
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,11 +1,11 @@
-import PropTypes from "prop-types";
-import s from "./Contacts.module.css";
-import ContactItem from "../ContactItem/ContactItem";
+import PropTypes from 'prop-types';
+import s from './Contacts.module.css';
+import ContactItem from '../ContactItem/ContactItem';
 
 function Contacts({ listContacts = [], onDelete }) {
   return (
     <ul className={s.list}>
-      {listContacts.map((el) => (
+      {listContacts.map(el => (
         <li className={s.item} key={el.id}>
           <ContactItem
             id={el.id}
@@ -25,7 +25,7 @@ Contacts.propTypes = {
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
-    })
+    }),
   ),
   onDelete: PropTypes.func.isRequired,
 };
